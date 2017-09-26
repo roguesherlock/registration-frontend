5f822b5bfce8443a1bd766dbc540068c840baa3c
Coder3 <Coder3@amp.local>
2017-09-26 21:14:40 +0530

    Google form link for Food Mela

--- src/js/components/registration/thanks.component.js
+++ src/js/components/registration/thanks.component.js
@@ -9,6 +9,30 @@ class ThankCtrl {
         vm.formatDate = function(date) {
             return moment(date).format("Do MMM YYYY")
         }
+
+        vm.getAge = function(doB) {
+            return vm.calculateAge(moment(doB, "YYYY-MM-DD").toDate(), new Date());
+        }
+
+        vm.calculateAge = function(dateOfBirth, dateToCalculate) {
+            var calculateYear = dateToCalculate.getFullYear();
+            var calculateMonth = dateToCalculate.getMonth();
+            var calculateDay = dateToCalculate.getDate();
+
+            var birthYear = dateOfBirth.getFullYear();
+            var birthMonth = dateOfBirth.getMonth();
+            var birthDay = dateOfBirth.getDate();
+
+            var age = calculateYear - birthYear;
+            var ageMonth = calculateMonth - birthMonth;
+            var ageDay = calculateDay - birthDay;
+
+            if (ageMonth < 0 || (ageMonth == 0 && ageDay < 0)) {
+                age = parseInt(age) - 1;
+            }
+            return age;
+        }
+
         let registrationListener = $rootScope.$on('registered', function(event, data) {
             $scope.savedData = data;
             $scope.current_event = _.find(vm.events, {
